c1204f8b302eb8d04ba24e0b3f1f19b0a53b463d
Paulo <Paulo@DESKTOP-FT12GQD>
2021-03-17 21:06:23 -0400

    Criado classe BaseTeste para realizar testes com banco em memória

---
+++ src/Tests/BaseTeste.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Infra;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests
+{
+    public class BaseTeste : IDisposable
+    {
+        protected readonly ApplicationContext context;
+
+        protected BaseTeste()
+        {
+            context = new ApplicationContext(ObterConfiguracoes());
+        }
+
+        private static DbContextOptions<ApplicationContext> ObterConfiguracoes()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var builder = new DbContextOptionsBuilder<ApplicationContext>();
+            builder.UseInMemoryDatabase("DbTeste")
+                   .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+
+        public void Dispose() => context.Dispose();
+    }
+}
