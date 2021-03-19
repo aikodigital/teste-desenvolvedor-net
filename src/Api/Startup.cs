e7ec2c43a29352eb5faeebe982b0a921ea5b50b2
Paulo <Paulo@DESKTOP-FT12GQD>
2021-03-19 10:47:05 -0400

    Configurando swagger para acesso em prod

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -59,9 +59,10 @@ namespace Api
         {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
             }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 
             app.UseHttpsRedirection();
 
